cdddae7dfbd6310c21ab2844a7ba3fa84bd05e22
Edonisselmanii <edonisselmani12@gmail.com>
2025-01-19 14:31:35 +0100

    Landing Page

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    if (name && email && message) {
+      alert(`Thank you, ${name}! We have received your message.`);
+      this.reset(); // Reset form
+    } else {
+      alert('Please fill out all fields.');
+    }
+  });
+  
